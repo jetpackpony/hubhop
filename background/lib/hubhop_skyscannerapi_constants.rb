1296b0b75615e8400f50816219159425b9edb946
jetpackpony <jetpackpony@gmail.com>
2016-09-08 23:21:05 +0300

    Add SkyScanner API implementation

---
+++ background/lib/hubhop_skyscannerapi_constants.rb
@@ -0,0 +1,9 @@
+module HubHop
+  module SkyScannerAPI
+    module Constants
+      SKYSCANNER_API = "http://partners.api.skyscanner.net/apiservices"
+      BROWSE_CACHE_ADDRESS = "#{SKYSCANNER_API}/browsequotes/v1.0"
+      CREATE_PRICING_SESSION_ADDRESS = "#{SKYSCANNER_API}/pricing/v1.0"
+    end
+  end
+end

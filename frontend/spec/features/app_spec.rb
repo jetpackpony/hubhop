0477edfe238890440bc592d392f7b407539befef
jetpackpony <jetpackpony@gmail.com>
2016-10-04 18:56:21 +0300

    Add initial form and test for it

--- frontend/spec/features/app_spec.rb
+++ frontend/spec/features/app_spec.rb
@@ -1,6 +1,11 @@
 describe "HubHop app", type: :feature do
   it "shows the form" do
     visit "/"
-    expect(page).to have_content "Hello, world"
+    expect(page).to have_content "Search request form"
+    expect(page).to have_button "Submit"
   end
+
+  context "errors in form validation"
+  context "failed to create a request"
+  context "created request successfully"
 end
